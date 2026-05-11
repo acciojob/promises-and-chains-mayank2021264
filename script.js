cba679243ba0aec2b44ae8456a381c036a1d9fee
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-05-11 17:23:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+let submit = document.getElementById('btn');
+let form = document.getElementById('myForm');
+
+function PromiseChaining(event){
+	event.preventDefault();
+	let Uage = parseInt(document.getElementById('age').value);
+	let Uname = document.getElementById('name').value;
+	if(!Uage || !Uname){
+		alert("Please enter valid details.")
+	}
+	let myPromise = new Promise((resolve, reject) => {
+		if(Uage >=18){
+			setTimeout(() => {
+				resolve("Welcome, You can vote.");
+			},4000);
+		}
+		else{
+			setTimeout(() => {
+				resolve("Oh sorry . You aren't old enough.");
+			},4000);
+		}
+	});
+
+	myPromise.then((message) => {
+		alert(message);
+		console.log("promise resolved");
+	}).catch((err) => {
+		console.log("error", err);
+	});
+}
+
+form.addEventListener('submit', PromiseChaining);
